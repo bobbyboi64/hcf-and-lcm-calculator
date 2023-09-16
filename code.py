43f6e482d90ee4b270066a3c1ac7ceeb3daeca80
bobbyboi64 <104128215+bobbyboi64@users.noreply.github.com>
2023-09-16 12:16:10 +1000

    converted to a function

--- code.py
+++ code.py
@@ -1,17 +1,32 @@
-first = int(input('first number'))
-second = int(input('second number'))
-if second > first:
-  a = second
-  b = first
-else:
-  a = first
-  b = second
-while True:
-  if a%b == 0:
-    print('hcf is '+ str(b))
-    print('lcm is '+str(first*second//b))
-    break
-  x = a%b
-  print(x)
-  a = b
-  b = x
+def hcf(a,b):
+    first = a
+    second = b
+    if second > first:
+        a = second
+        b = first
+    else:
+        a = first
+        b = second
+    while True:
+        if a%b == 0:
+            print('hcf is '+ str(b))
+            return(b)
+        x = a%b
+        a = b
+        b = x
+
+def lcm(a,b):
+    first = a
+    second = b
+    if second > first:
+        a = second
+        b = first
+    else:
+        a = first
+        b = second
+    while True:
+        if a%b == 0:
+            return(first*second//b)
+        x = a%b
+        a = b
+        b = x
